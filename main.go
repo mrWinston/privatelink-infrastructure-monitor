56eadc24f540fe429a7529b1c79b5d9b617fed07
Florian Bergmann <fbergman@redhat.com>
2022-06-14 14:24:30 +0200

    Add new metric 'InterfaceEndpointsPerVPC'

--- main.go
+++ main.go
@@ -70,6 +70,11 @@ func main() {
 		Ec2Client:          ec2Client,
 		VpcID:              *vpcId,
 	})
+	allCollectors = append(allCollectors, &collectors.InterfaceVpcEndpointsPerVpc{
+		ServiceQuotaClient: serviceQuotaClient,
+		Ec2Client:          ec2Client,
+		VpcID:              *vpcId,
+	})
 
 	for _, col := range allCollectors {
 		quota, err := col.Quota()
--- pkg/collectors/vpcCollectors.go
+++ pkg/collectors/vpcCollectors.go
@@ -11,11 +11,40 @@ import (
 )
 
 const (
-	QUOTA_ROUTE_TABLES_PER_VPC     string = "L-589F43AA"
-	QUOTA_ROUTES_PER_ROUTE_TABLE   string = "L-93826ACB"
-	QUOTA_CODE_IPV4_BLOCKS_PER_VPC string = "L-83CA0A9D"
-	SERVICE_CODE_VPC               string = "vpc"
+	QUOTA_INTERFACE_VPC_ENDPOINTS_PER_VPC string = "L-29B6F2EB"
+	QUOTA_ROUTE_TABLES_PER_VPC            string = "L-589F43AA"
+	QUOTA_ROUTES_PER_ROUTE_TABLE          string = "L-93826ACB"
+	QUOTA_CODE_IPV4_BLOCKS_PER_VPC        string = "L-83CA0A9D"
+	SERVICE_CODE_VPC                      string = "vpc"
 )
+
+type InterfaceVpcEndpointsPerVpc struct {
+	ServiceQuotaClient *servicequotas.Client
+	Ec2Client          *ec2.Client
+	VpcID              string
+}
+
+func (c InterfaceVpcEndpointsPerVpc) Quota() (float64, error) {
+	return GetQuotaValue(c.ServiceQuotaClient, SERVICE_CODE_VPC, QUOTA_INTERFACE_VPC_ENDPOINTS_PER_VPC)
+}
+
+func (c InterfaceVpcEndpointsPerVpc) Usage() (float64, error) {
+	describeVpcEndpointsOutput, err := c.Ec2Client.DescribeVpcEndpoints(context.TODO(), &ec2.DescribeVpcEndpointsInput{
+		Filters: []types.Filter{{
+			Name:   aws.String("vpc-id"),
+			Values: []string{c.VpcID},
+		}},
+	})
+	if err != nil {
+		return 0, err
+	}
+	numEndpoints := len(describeVpcEndpointsOutput.VpcEndpoints)
+	return float64(numEndpoints), nil
+}
+
+func (c InterfaceVpcEndpointsPerVpc) Name() string {
+	return "interface_vpc_endpoints_per_vpc_" + c.VpcID
+}
 
 type RoutesPerRouteTableCollector struct {
 	ServiceQuotaClient *servicequotas.Client
